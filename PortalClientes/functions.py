13f2492e9f431d1a15d315730144a5c0dbbd5e1e
JoaquinDucos <87881536+JoaquinDucos@users.noreply.github.com>
2022-11-12 19:46:09 -0300

    Updated new functions

--- PortalClientes/functions.py
+++ PortalClientes/functions.py
@@ -37,3 +37,49 @@ def is_password_valid(password):
         return True
      else:
         return False
+
+def split(S, c):
+
+    lista=[]
+    sub=""
+
+    for i in S:
+        if i != c:
+            sub += i  
+        else:
+            lista.append(sub)
+            sub = ""    
+
+    if sub != "":
+        lista.append(sub) 
+            
+    return lista
+
+def transformar_archivo_DB(archivo):
+    try:    
+        archivo = open (archivo, "r")
+        matriz = split (archivo.read(), "\n") 
+        matriz_aux= []
+
+        for fila in matriz:
+            fila = split (fila, " ")
+            matriz_aux.append(fila) 
+        archivo.close ()  
+
+        return matriz_aux
+
+    except FileNotFoundError:
+        print("No existe el archivo 'ventas.txt', crearemos uno nuevo\n")  
+        matrizDB = []
+
+        return matrizDB           
+
+def transform_to_str(matriz):
+    matriz_nuev= []
+
+    for fila in matriz:
+        cadena = ''
+        for elemento in fila:
+            cadena += str(elemento)+' '
+        matriz_nuev.append(cadena)    
+    return matriz_nuev                  
